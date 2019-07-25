d337a3c02352207434ebd8d2534a2eabaf288c13
nano1237 <tim.ruecker@ymail.com>
2019-07-25 20:36:56 +0200

    updating the meta files for correct package

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import {PLANCK_CONSTANT} from "./constants";
+export {FaradPerMetre} from "./relative-units/farad-per-metre";
+export {JoulesPerSecond} from "./relative-units/joules-per-second";
+export {MetresPerSecond} from "./relative-units/metres-per-second";
 
-console.log(PLANCK_CONSTANT.forSeconds(1));
+export {Farad} from "./derived-units/farad";
+export {Joule} from "./derived-units/joule";
+export {Lumen} from "./derived-units/lumen";
+export {Radian} from "./derived-units/radian";
+export {Steradiant} from "./derived-units/steradiant";
+export {Watt} from "./derived-units/watt";
+
+export {Ampere} from "./base-units/ampere";
+export {Candela} from "./base-units/candela";
+export {Kelvin} from "./base-units/kelvin";
+export {Kilogram} from "./base-units/kilogram";
+export {Metre} from "./base-units/metre";
+export {Mole} from "./base-units/mole";
+export {Second} from "./base-units/second";
+
+export {BOHR_RADIUS, PLANCK_CONSTANT, SPEED_OF_LIGHT, VACUUM_PERMITTIVITY} from "./constants";
