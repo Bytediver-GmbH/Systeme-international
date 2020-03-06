ee446228a30699f9c5bad8e34f93a9ec146941ef
nano1237 <tim.ruecker@ymail.com>
2020-03-06 10:37:19 +0100

    adding the avogadro constant

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,7 @@ import {MetresPerSecond} from "./relative-units/metres-per-second";
 
 export const BOHR_RADIUS: Metre = new Metre(5.29e-11);
 export const FINE_STRUCTURE_CONSTANT = "4π × 1.00000000082(20)×10^−7 H⋅m^−1";
+export const AVOGADRO_CONSTANT = "6,022 140 76 · 1023 mol−1";
 export const GRAVIATION = "6.674×10^−11 m^3⋅kg^−1⋅s^−2";
 export const PLANCK_CONSTANT = new JoulesPerSecond(6.62607015e-34);
 export const SPEED_OF_LIGHT = new MetresPerSecond(22979248);
